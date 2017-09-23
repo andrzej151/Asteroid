3b6980d268779baa476dfabe5f32fad30267cdaf
Andrzej Dabrowski <andrzej.dabrowski94@gmail.com>
2017-09-24 01:50:18 +0200

    AS-002 Ship constructor and draw

--- aster/js/game.js
+++ aster/js/game.js
@@ -22,6 +22,8 @@ Game = {
         window.addEventListener('resize', Game.layout, false);
         
         document.body.appendChild(Game.canvas);
+        Game.ship = new Ship();
+        
         Game.animationLoop();
     },
     layout:function(ev){
@@ -42,10 +44,9 @@ Game = {
         requestAnimationFrame (Game.animationLoop);
         if(time-VAR.lastTime>=1000/VAR.fps){
             VAR.lastTime =time;
-            
-            console.log("loop");
+            //console.log("loop");
             Game.ctx.clearRect(0,0,VAR.W,VAR.H);
-            
+            Game.ship.draw();
         }
     }
 };
